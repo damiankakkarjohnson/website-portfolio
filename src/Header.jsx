77780648ae7c4d8a546e126e903d83655fb5e373
Damian Kakkar-Johnson <damiankakkarjohnson@gmail.com>
2026-06-03 13:15:44 -0400

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import NavBar from './NavBar.jsx'
+
+function Header() {
+
+  return (
+    <header id='header'>
+      <h1>Damian Kakkar-Johnson</h1>
+      <NavBar/>
+    </header>
+  );
+}
+
+export default Header
---
+++ src/NavBar.jsx
@@ -0,0 +1,18 @@
+
+function NavBar(){
+  
+  return(
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Leadership</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar
