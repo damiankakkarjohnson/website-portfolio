d5ecff270581eaa444f8c7ca291594ed189da448
damiankakkarjohnson <damiankakkarjohnson@gmail.com>
2026-06-04 20:12:01 -0400

    Some experience entries added

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,6 +4,21 @@ function Experience() {
     return(
         <div id="experience">
             <h2>Experience</h2>
+
+            <br></br>
+            <div id="experienceEntries">
+                <div className="experienceEntry">
+                    <h3>Co-Founder</h3>
+                    <h4>Aisslo LLC</h4>
+                    <p className="experienceTime">October 2025 - Present</p>
+                </div>
+
+                <div className="experienceEntry">
+                    <h3>Crew Member</h3>
+                    <h4>Chipotle Mexican Grill</h4>
+                    <p className="experienceTime">June 2025 - December 2025</p>
+                </div>
+            </div>
         </div>
     );
 }
